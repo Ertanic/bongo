79dc300a042121cfbc64581a082f971bbde3007d
Ertanic <black.ikra.14@gmail.com>
2025-06-03 21:26:34 +0300

    feat: config file has been added

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use anyhow::Context;
+use crate::config::load_config;
 use crate::logs::init_logs;
+use anyhow::Context;
 
+mod config;
 mod logs;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let _guard = init_logs().await.context("Failed to init logs system")?;
+    let config = load_config("config.kdl".as_ref()).await?;
+    let _guard = init_logs(&config.logs)
+        .await
+        .context("Failed to init logs system")?;
 
     tracing::info!("Application is starting...");
     tracing::info!("Application is running");
