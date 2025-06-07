f0d907e5d0540e1454582ceff3a29c988b95c2c7
Ertanic <black.ikra.14@gmail.com>
2025-06-07 14:07:09 +0300

    build: copy_to_output replaced by copy-glob

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
-use copy_to_output::copy_to_output;
-use std::env;
+use copy_glob::{CopyGlobOptionsBuilder, copy_glob_with, get_root_path, get_target_folder};
 
 fn main() {
-    println!("cargo:rerun-if-changed=resources/*");
-    copy_to_output("resources/config.kdl", &env::var("PROFILE").unwrap()).expect("Could not copy");
+    let output = get_target_folder();
+    let resources = get_root_path().join("resources");
+    let copy_options = CopyGlobOptionsBuilder::new()
+        .set_root_path(resources)
+        .build();
+    copy_glob_with("*", output, &copy_options);
 }
