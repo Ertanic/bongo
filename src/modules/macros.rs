cba5951ba49206c4133c1d32521b226f152da121
Ertanic <black.ikra.14@gmail.com>
2025-06-10 11:55:43 +0300

    feat(modules): add other logging levels

--- src/modules/macros.rs
+++ src/modules/macros.rs
@@ -3,6 +3,57 @@ use rune::macros::{quote, FormatArgs, MacroContext, TokenStream};
 use rune::parse::Parser;
 use rune::runtime::VmResult;
 
+#[rune::macro_(path = error)]
+pub fn error_macro(
+    cx: &mut MacroContext<'_, '_, '_>,
+    stream: &TokenStream,
+) -> compile::Result<TokenStream> {
+    let mut p = Parser::from_token_stream(stream, cx.input_span());
+    let args = p.parse_all::<FormatArgs>()?;
+    let expanded = args.expand(cx)?;
+    Ok(quote!(error(#expanded)).into_token_stream(cx)?)
+}
+
+#[rune::function(path = error)]
+pub fn error_impl(message: &str) -> VmResult<()> {
+    tracing::error!(message);
+    VmResult::Ok(())
+}
+
+#[rune::macro_(path = warn)]
+pub fn warn_macro(
+    cx: &mut MacroContext<'_, '_, '_>,
+    stream: &TokenStream,
+) -> compile::Result<TokenStream> {
+    let mut p = Parser::from_token_stream(stream, cx.input_span());
+    let args = p.parse_all::<FormatArgs>()?;
+    let expanded = args.expand(cx)?;
+    Ok(quote!(warn(#expanded)).into_token_stream(cx)?)
+}
+
+#[rune::function(path = warn)]
+pub fn warn_impl(message: &str) -> VmResult<()> {
+    tracing::warn!(message);
+    VmResult::Ok(())
+}
+
+#[rune::macro_(path = info)]
+pub fn info_macro(
+    cx: &mut MacroContext<'_, '_, '_>,
+    stream: &TokenStream,
+) -> compile::Result<TokenStream> {
+    let mut p = Parser::from_token_stream(stream, cx.input_span());
+    let args = p.parse_all::<FormatArgs>()?;
+    let expanded = args.expand(cx)?;
+    Ok(quote!(info(#expanded)).into_token_stream(cx)?)
+}
+
+#[rune::function(path = info)]
+pub fn info_impl(message: &str) -> VmResult<()> {
+    tracing::info!(message);
+    VmResult::Ok(())
+}
+
 #[rune::macro_(path = debug)]
 pub fn debug_macro(
     cx: &mut MacroContext<'_, '_, '_>,
@@ -17,5 +68,22 @@ pub fn debug_macro(
 #[rune::function(path = debug)]
 pub fn debug_impl(message: &str) -> VmResult<()> {
     tracing::debug!(message);
+    VmResult::Ok(())
+}
+
+#[rune::macro_(path = trace)]
+pub fn trace_macro(
+    cx: &mut MacroContext<'_, '_, '_>,
+    stream: &TokenStream,
+) -> compile::Result<TokenStream> {
+    let mut p = Parser::from_token_stream(stream, cx.input_span());
+    let args = p.parse_all::<FormatArgs>()?;
+    let expanded = args.expand(cx)?;
+    Ok(quote!(trace(#expanded)).into_token_stream(cx)?)
+}
+
+#[rune::function(path = trace)]
+pub fn trace_impl(message: &str) -> VmResult<()> {
+    tracing::trace!(message);
     VmResult::Ok(())
 }
